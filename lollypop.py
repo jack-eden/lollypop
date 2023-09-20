6b6ceb03e8d85eb8cbc4f5d1081847db78df8b91
Jack Oliver <j@jackoliver.xyz>
2023-09-20 06:11:15 +0100

    md5sum working for all files in tree

--- lollypop.py
+++ lollypop.py
@@ -1,4 +1,5 @@
 import os
+import hashlib
 
 def get_fullpath():
     path = os.getcwd()
@@ -10,15 +11,25 @@ def get_parent(path):
     segments = path.split("/")
     return segments[-1]
 
+def get_md5sum(filepath):
+    BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
+    md5 = hashlib.md5()
+    with open(filepath, 'rb') as f:
+        while True:
+            data = f.read(BUF_SIZE)
+            if not data:
+                break
+            md5.update(data)
+    md5sum = md5.hexdigest()
+    return md5sum
+
 def walk_path(path):
     for (root,dirs,files) in os.walk(path, topdown=True):
         for file in files:
+            filepath = "/".join([root,file])
             parent = get_parent(root)
-            print(f"{parent}/{file}, {root}/{file}")
-
-
-def outfile(output):
-    pass
+            md5sum = get_md5sum(filepath)
+            print(f"{md5sum}, {filepath}, {parent}/{file}")
 
 def main():
     fullpath = get_fullpath()
