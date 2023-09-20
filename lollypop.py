830b80e094c5493196f723fcaeb9fde14d953f44
Jack Oliver <j@jackoliver.xyz>
2023-09-20 02:58:10 +0100

    inital plan

---
+++ lollypop.py
@@ -0,0 +1,43 @@
+import argparse
+
+# globals
+name = ""
+fullpath = ""
+
+def get_args():
+    #get directory nickname as a string
+    #get directory path as a string
+    #ignored files: for now we will always ignore .files no matter what, maybe in the future I can add something more sophisticated
+    #return args as a list
+    pass
+
+def get_dir():
+    #check that directory path string is an absolute path
+    #return absolute path as a string
+    pass
+
+def get_dir_contents():
+    #list all files and folders in a given directory
+    #break list into two seperate lists for files and folders
+    #return as a list: [ [folders], [files] ]
+    pass
+
+
+
+def outfile():
+    #print string to console
+    pass
+
+def main():
+    #get args
+    #parse name out of args
+    #update global name
+    #parse full directory path out of args
+    #update global fullpath
+    #walking: set depth 0
+    #walking: use fullpath to get dir contents
+    #walking: 
+    print(rootpath)
+
+if __name__ == "__main__":
+    main()
